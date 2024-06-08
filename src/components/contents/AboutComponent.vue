e52e563f7cd31a9943862f87fbe43ac7f3e4f609
orlan331 <orlansagala36@gmail.com>
2024-06-08 13:07:09 +0700

    second commit

---
+++ src/components/contents/AboutComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="slide-right w-full rounded-md bg-background sm:p-5 max-sm:p-3 shadow-custom">
+        <h1 class="sm:text-xl max-sm:text-lg font-semibold mb-3"><span class="text-title">About</span> Me</h1>
+        <p class="leading-relaxed text-justify max-sm:text-sm">
+           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium ad voluptates recusandae aliquid. Laborum non, nemo temporibus cupiditate commodi est harum. Odit consequuntur, voluptatem laudantium ducimus dolore quis vel libero. Provident veniam reiciendis id. At quasi qui esse quae, tempore voluptate doloremque, facilis vero id officia velit quia. At fugit ratione fugiat, quis similique ab dolorem pariatur aspernatur assumenda dolore consequatur inventore consequuntur beatae molestias ipsam iste adipisci ex quasi eos blanditiis! Possimus ipsa provident laudantium doloribus atque dolorum a obcaecati tempore eaque nostrum molestiae adipisci, excepturi impedit perspiciatis delectus voluptatem in blanditiis, officiis, ab fugit! Harum eius aperiam dolorum!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'about-component',
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./indexedDB.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors:{
+        'background' : '#29241e',
+        'text': '#f7f9fa',
+        'title' : '#4cb6f7',
+        'title-hover': '#5ca8ff',
+        'bg': '#1d2120',
+    },
     extend: {},
   },
   plugins: [],
